e4d13da0b60dee133b697e0567e0faa05c7da85e
Omeko-FG <gurbuzzomer@gmail.com>
2023-07-18 07:04:23 +0300

    tecnologies added

--- my-website/components/Technologies.jsx
+++ my-website/components/Technologies.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 
 const Tecnologies = () => {
   return (
-    <div class="grid grid-cols-1 md:grid-cols-2 mt-24">
-      <div class='text-white'><p class="flex text-3xl font-semibold justify-center">Technologies I Use</p></div>
-      <div class=" grid-cols-2">
-        <div class=""> 
-        <img class="h-14" src="https://www.vhv.rs/dpng/d/503-5034990_react-js-project-png-download-white-react-native.png" alt="text-red-500" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+    <div class="mt-24">
+      <div class='text-white m-20'><p class="flex text-3xl font-semibold justify-center">Technologies I Use</p></div>
+        <div class="flex flex-wrap gap-8 m-4 justify-center items-center"> 
+        <img class="w-44" src="https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_wordmark_logo_icon_146375.png" alt="" />
+        <img class="w-44" src="https://www.datocms-assets.com/75941/1657707878-nextjs_logo.png" alt="" />
+        <img class="w-44" src="https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png" alt="" />
+        <img class="w-44" src="https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png" alt="" />
+        <img class="w-44" src="https://www.svgrepo.com/show/373554/django.svg" alt="" />
+        <img class="w-44" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png" alt="" />
+        <img class="w-44" src="https://cdn.icon-icons.com/icons2/2415/PNG/512/mysql_original_wordmark_logo_icon_146417.png" alt="" />
+        <img class="w-44" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1985px-Postgresql_elephant.svg.png" alt="" />
+        <img class="w-44" src="https://user-images.githubusercontent.com/25181517/183898054-b3d693d4-dafb-4808-a509-bab54cf5de34.png" alt="" />
+        <img class="w-44" src="https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png" alt="" />
+        <img class="w-44" src="https://user-images.githubusercontent.com/25181517/189716630-fe6c084c-6c66-43af-aa49-64c8aea4a5c2.png" alt="" />
+        <img class="w-44" src="https://user-images.githubusercontent.com/25181517/189716855-2c69ca7a-5149-4647-936d-780610911353.png" alt="" />
+        <img class="w-44" src="https://www.svgrepo.com/show/349375/github.svg" alt="" />
         </div>
-        <div>
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        </div>
-      </div>
     </div>
   )
 }
